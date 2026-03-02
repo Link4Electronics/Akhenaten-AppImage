9324ebc65125981254a35c2607cf1f18190a53fc
Link4Electronics <link4electronics@gmail.com>
2026-03-01 23:42:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,8 +19,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#export CXXFLAGS="${CXXFLAGS:-} -Wno-error=format-security"
-#make-aur-package akhenaten-git
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 
